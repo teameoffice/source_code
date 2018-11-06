ecaeaf87d623f81e62065efd3bfb58dd6dfa03b3
Timur Yulis <timuryulis14@gmail.com>
2018-11-06 19:46:01 +0700

    Modul workflow

--- application/controllers/Workflow.php
+++ application/controllers/Workflow.php
@@ -8,14 +8,159 @@ class Workflow extends CI_Controller {
                 // Your own constructor code
                 $this->load->database();
                 $this->load->helper('login_helper');
+        date_default_timezone_set('Asia/Jakarta');       
     }
 	public function index()
 	{
 		if(_is_user_login($this)){
             $data = array();
+            $this->load->model("workflow_model");
+            $data["workflow"] = $this->workflow_model->get_workflow_filter_by_flag_del();
 
             $this->load->view("workflow/index",$data);
         }
+    }
+
+
+public function add_workflow(){
+           
+        if(_is_user_login($this)){
+        
+            $data["error"] = "";
+            $this->load->model("workflow_model");
+
+            if($_POST){
+               
+                $this->load->library('form_validation');
+                
+                $this->form_validation->set_rules('nama_workflow', 'Nama Workflow', 'trim|required');
+                if ($this->form_validation->run() == FALSE) 
+                {
+                  
+                $error_array = $this->form_validation->error_string();
+                $error_json = json_encode(strip_tags($error_array));
+                $error_clean = str_replace(['"', '"'], '', $error_json);
+
+                
+               
+                    $data["error"] =  "<script type='text/javascript'>
+                                                    var teks = ' ".$error_clean."';
+                                                    swal({   
+                                                            title: '',   
+                                                            text: teks,   
+                                                            type: 'warning',   
+                                                            showCancelButton: false,   
+                                                            confirmButtonColor: '#DD6B55',   
+                                                            confirmButtonText: 'OK', 
+                                                            closeOnConfirm: false 
+                                                        });
+                                            </script>";
+
+
+                    
+                } else
+               {
+                        
+                        $nama_workflow = $this->input->post("nama_workflow");
+                        
+                      //  print_r($nama_workflow);die();
+                   
+                        
+                            $this->load->model("common_model");
+                            $this->common_model->data_insert("workflow",
+                                array(
+                                "nama_workflow"=>$nama_workflow,
+                                "date_created"=>date("Y-m-d H:i:sa"),
+                                "date_updated"=>date("Y-m-d H:i:sa"),
+                                "flag_del"=>0));
+                            echo  '<script>alert("Data berhasil disimpan...");window.location = "'.site_url().'/workflow/";</script>
+                                ';
+
+
+                            $data["error"] =  "<script type='text/javascript'>
+                                                    swal('Sukses!', 'Data berhasil disimpan', 'success')
+                                            </script>";
+                        
+                }
+            }
+            
+            
+           // print_r($data["user"]);die();
+            $this->load->view("workflow/add_workflow");
+        }
+    }
+
+
+public function edit_workflow($id_workflow){
+    
+        if(_is_user_login($this)){
+            $data = array();
+            $this->load->model("workflow_model");            
+            $workflow = $this->workflow_model->get_workflow_by_id($id_workflow);                  
+            $data["workflow"] = $workflow;
+            //print_r($data["user"]);die();
+            if($_POST){
+                $this->load->library('form_validation');
+                
+                $this->form_validation->set_rules('nama_workflow', 'Nama Workflow', 'trim|required');                
+                if ($this->form_validation->run() == FALSE) 
+                {
+                  
+                $error_array = $this->form_validation->error_string();
+                $error_json = json_encode(strip_tags($error_array));
+                $error_clean = str_replace(['"', '"'], '', $error_json);
+
+                
+               
+                    $data["error"] =  "<script type='text/javascript'>
+                                                    var teks = ' ".$error_clean."';
+                                                    swal({   
+                                                            title: '',   
+                                                            text: teks,   
+                                                            type: 'warning',   
+                                                            showCancelButton: false,   
+                                                            confirmButtonColor: '#DD6B55',   
+                                                            confirmButtonText: 'OK', 
+                                                            closeOnConfirm: false 
+                                                        });
+                                            </script>";
+                    
+               }else
+                {
+                        
+                       $nama_workflow = $this->input->post("nama_workflow");
+                       // print_r(date("Y-m-d H:i:s"));die();
+                        $update_array = array(
+                                "nama_workflow"=>$nama_workflow,                    
+                                "date_updated"=>date("Y-m-d H:i:s"));
+                       // print_r($update_array);print_r($user_id); die();
+                       echo  '<script>alert("Data berhasil disimpan...");window.location = "'.site_url().'/workflow/";</script>
+                                ';
+                        
+                            $this->load->model("common_model");
+                            $this->common_model->data_update("workflow",$update_array,array("id"=>$id_workflow)
+                                );
+                            $data["error"] =  "<script type='text/javascript'>
+                                                    swal('Sukses!', 'Data berhasil disimpan', 'success')
+                                            </script>";
+                        
+                }
+            }
+            
+            
+            $this->load->view("workflow/edit_workflow",$data);
+        }
+    }
+
+     function delete_workflow($id_workflow){
+        $data = array();
+            $this->load->model("workflow_model");
+           $workflow = $this->workflow_model->get_workflow_by_id($id_workflow);
+           if($workflow){
+                $this->db->query("update workflow set flag_del = 1 where id = '".$workflow->id."'");
+                echo  '<script>alert("Data berhasil dihapus...");window.location = "'.site_url().'/workflow/";</script>
+                                ';
+           }
     }
 
 }
---
+++ application/models/Workflow_model.php
@@ -0,0 +1,23 @@
+<?php 
+class Workflow_model extends CI_Model{
+
+  public function get_workflow_filter_by_flag_del(){
+        
+        $this->db->select();
+        $this->db->from('workflow');
+        $this->db->where('flag_del',0);
+        //$this->db->where_not_in('id',_get_current_user_id($this));
+        $q = $this->db->get();
+        return $q->result();
+        print_r($q);
+        die();
+    }
+
+ public function get_workflow_by_id($id){
+        $q = $this->db->query("select * from workflow where  id = '".$id."' limit 1");
+        return $q->row();
+    }
+
+   
+}
+ ?>
