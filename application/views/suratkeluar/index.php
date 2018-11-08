22029296cbb534b34ea738be0a3e1c79da3d6ac3
ahmadshd <ahmadshudi@gmail.com>
2018-11-08 09:34:40 +0700

    data tabel surat masuk dan surat keluar

--- application/views/suratkeluar/index.php
+++ application/views/suratkeluar/index.php
@@ -12,15 +12,92 @@
 
 
         <div class="mn-content fixed-sidebar">
-            <?php  $this->load->view("admin/common/common_header"); ?>
+            <?php  $this->load->view("front/common/common_header"); ?>
 
-            <?php  $this->load->view("admin/common/common_sidebar"); ?>
-            <main class="mn-inner inner-active-sidebar">
-                    Silahkan design disini
+            <?php  $this->load->view("front/common/common_sidebar"); ?>
+            <main class="mn-inner">
+                <div class="row">
+                    <div class="col s12">
+                        <div class="page-title"></div> 
+
+                    </div>
+                    <div class="col s12 m12 l12">
+                        <div class="card">
+                            <div class="card-content">
+                              <a href="<?php echo site_url("users/add_user/"); ?> "
+                                 class="btn-floating btn-medium waves-effect waves-light right">
+                                 <i class="material-icons">add</i></a>
+                                <span class="card-title">SURAT KELUAR</span> 
+                                <table id="example" class="display responsive-table datatable-example">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nomor Surat</th>
+                                            <th>Nama Dokumen</th>
+                                            <th>Nama Alur</th>
+                                            <th>Deskripsi</th>
+                                            <th>Tipe Dokumen</th>
+                                            <th>Disetujui</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    
+                                    <tbody>
+                                        
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td>></td>
+                                            <td>
+                                                <div class="switch m-b-md ">
+                                                    <label for='cb_<?php echo $user->user_id; ?>'>
+                                                      <input type="checkbox" class="tgl_checkbox"
+                                                       data-table="users" 
+                                                       data-status="user_status" 
+                                                       data-idfield="user_id"
+                                                       data-id="" 
+                                                       id=''
+                                                       
+                                                      <span class="lever"></span>
+                                                    </label>
+                                                </div>
+                                            </td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td>
+                                                <a href="">
+                                                <i class="material-icons">mode_edit</i></a>
+                                                &nbsp;&nbsp;&nbsp;&emsp;
+                                               <a href="" onclick="return confirm('are you sure to delete?')" class=""> 
+
+                                               <!--  <a class="waves-effect waves-light m-b-xs sweetalert-warning"> -->
+                                                <i class="material-icons">delete_forever</i></a>
+                                            </td>
+                                        </tr>
+                                        
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nomor Surat</th>
+                                            <th>Nama Dokumen</th>
+                                            <th>Nama Alur</th>
+                                            <th>Deskripsi</th>
+                                            <th>Tipe Dokumen</th>
+                                            <th>Disetujui</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
-           <?php  $this->load->view("admin/common/common_footer"); ?>
+           <?php  $this->load->view("front/common/common_footer"); ?>
         </div>
-        <div class="left-sidebar-hover"></div>      
+        <div class="left-sidebar-hover"></div>
 
 
         <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/jquery/jquery-2.2.0.min.js"); ?>"></script>
