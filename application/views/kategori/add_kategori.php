0517447e261e1025b05329f2058b4c0715c9c7fa
ahmadshd <ahmadshudi@gmail.com>
2018-11-09 11:34:00 +0700

    kategori

---
+++ application/views/kategori/add_kategori.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php  $this->load->view("common/common_head"); ?>
+    <body>
+        
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/plugins/materialize/css/materialize.min.css"); ?>"> 
+        <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/plugins/material-preloader/css/materialPreloader.min.css"); ?>"> 
+        <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/plugins/datatables/css/jquery.dataTables.min.css"); ?>"> 
+
+        <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/plugins/sweetalert/sweetalert.css"); ?>">
+
+        <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/alpha.min.css"); ?>"> 
+        <link rel="stylesheet" href="<?php echo base_url($this->config->item("theme_admin")."/css/custom.css"); ?>"> 
+
+
+
+        <div class="mn-content fixed-sidebar">
+            <?php  $this->load->view("admin/common/common_header"); ?>
+
+            <?php  $this->load->view("admin/common/common_sidebar"); ?>           
+       
+    <main class="mn-inner">
+                <div class="row">
+                    <div class="col s12">
+                        <div class="page-title">Nama Kategori</div>
+                    </div>
+                    <div class="col s12 m12 l12">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="row">
+                                    <form class="col s12" action="" method="post">
+                                        <div class="row">
+                                            <div class="input-field col s6">                                               
+                                                <input placeholder="Nama Kategori" id="nama_workflow" type="text" class="validate" name="nama_workflow">
+                                                <label for="nama">Nama Kategori</label>
+                                            </div>
+                                            <div class="row">
+                                            <div class="input-field col s7">                                               
+                                                <input placeholder="Deskripsi" id="deskripsi" type="text" class="validate" name="deskripsi">
+                                                <label for="deskripsi">Deskripsi</label>
+                                            </div>                                              
+                                        </div>
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <button type="submit" name="submit" class="btn-floating btn-large waves-effect waves-light right"><i class="material-icons">save</i></button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+        </main>
+     
+    
+    </div>
+        <div class="left-sidebar-hover"></div>      
+
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/jquery/jquery-2.2.0.min.js"); ?>"></script>
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/materialize/js/materialize.min.js"); ?>"></script>
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/material-preloader/js/materialPreloader.min.js"); ?>"></script>
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/jquery-blockui/jquery.blockui.js"); ?>"></script>
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/datatables/js/jquery.dataTables.min.js"); ?>"></script>
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/js/alpha.min.js"); ?>"></script>
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/js/pages/table-data.js"); ?>"></script>
+
+        <script src="<?php echo base_url($this->config->item("theme_admin")."/plugins/sweetalert/sweetalert.min.js"); ?>"></script>
+         <script src="<?php echo base_url($this->config->item("theme_admin")."/js/pages/form_elements.js");?>"></script>
+          <script>
+            $(document).ready(function(){
+                $('#tanggal_lahir').pickadate({
+                    selectMonths: true, // Creates a dropdown to control month
+                    changeYear:true,
+                    yearRange: "1960:2020"
+                });
+
+                $('input.autocomplete').autocomplete({
+                    data: {
+                        "Apple": null,
+                        "Microsoft": null,
+                        "Google": 'assets/images/google.png'
+                    }
+                });
+            });
+        </script>
+
+        <? if(isset($error) && $error!=""){echo $error; } ?>
+       
+    </body>
+</html>
+            
