bae1f02d93a71b1eb50b9db20bf7d2abf1ac561c
fandeka <fandeka.aghfirlana@gmail.com>
2018-11-19 08:49:57 +0700

    default date dashboard with full calendar

--- theme/admin/js/pages/calendar.js
+++ theme/admin/js/pages/calendar.js
@@ -6,65 +6,10 @@ $( document ).ready(function() {
 				center: 'title',
 				right: 'month,agendaWeek,agendaDay'
 			},
-			defaultDate: '2015-12-12',
+			defaultDate: false,
 			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2015-12-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2015-12-07',
-					end: '2015-12-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-12-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-12-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2015-12-11',
-					end: '2015-12-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-12-12T10:30:00',
-					end: '2015-12-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2015-12-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-12-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2015-12-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2015-12-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2015-12-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2015-12-28'
-				}
-			]
+			eventLimit: true,
+			defaultDate: moment() 
 		});
 
 });
